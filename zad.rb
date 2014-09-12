4011fa95f11c43ee87454622b580d2a25323c4cb
Daniel Tredota <danieltredota@gmail.com>
2014-09-12 11:56:11 +0200

    druga petla

--- zad.rb
+++ zad.rb
@@ -2,7 +2,11 @@ def sortuj(tablica)
 licz = tablica.size-1
 
 while licz >= 0
-     
+     0.upto(tablica.size-2) do |i|
+         if tablica[i] > tablica[i+1]
+            tablica[i], tablica[i+1] = tablica[i+1], tablica[i]
+         end
+      end 
 licz = licz -1
 end
 
