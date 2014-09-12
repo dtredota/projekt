b98f88e9665a79cdee27cf24f0d43a36c2ff2f6d
Łukasz Maślej <lukasz.maslej@gmail.com>
2014-09-12 11:51:40 +0200

    Dodanie funkcji interface.

---
+++ lista.rb
@@ -0,0 +1,10 @@
+def print_interface
+
+puts "[1] Dodaj zadanie"
+puts "[2] Usuń zadanie"
+puts "[3] Wyświetl zadania"
+puts "[4] Edytuj zadania"
+puts "[5] Wyjście"
+print "Wybierz opcję:"
+return gets.chomp
+end
